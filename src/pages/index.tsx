1932cc4f8fedbfb6502f5041c7614e93658eccc3
Rodrigo Correia Braga <58756308+rodcorulfr@users.noreply.github.com>
2021-04-05 01:26:08 -0300

    Api react-youtube

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import YouTube from 'react-youtube';
 import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
+import React from 'react';
 
 let durationTimeout: NodeJS.Timeout;
 
@@ -13,7 +15,7 @@ export default function Home() {
 
   const totalTime = 1000;
 
-  const totalMinutes = Math.floor(totalTime/60);
+  const totalMinutes = Math.floor(totalTime / 60);
   const totalSeconds = totalTime % 60;
 
   const [totalMinuteLeft, totalMinuteRight] = String(totalMinutes).padStart(2, '0').split('');
@@ -25,16 +27,38 @@ export default function Home() {
   const [currentMinuteLeft, currentMinuteRight] = String(currentMinutes).padStart(2, '0').split('');
   const [currentSecondLeft, currentSecondRight] = String(currentSeconds).padStart(2, '0').split('');
 
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      listType: 'playlist',
+      list: 'PLe_sOLUv9qfuGAjO96WFXc5bgVaF46IjQ'
+    },
+  };
+
+  function pauseToPlay(){
+    stopToPlay();
+    
+  }
+
   function startToPlay() {
     setIsPlaying(true);
   }
 
   function stopToPlay() {
     setIsPlaying(false);
+    clearTimeout(durationTimeout);
   }
 
   function startNewMusic() {
     console.log('Nova musica');
+  }
+
+  function onReady(event){
+    event.target.playVideo();
+  }
+  function onPause(event){
+    event.target.playVideo();
   }
 
   useEffect(() => {
@@ -55,7 +79,11 @@ export default function Home() {
   }, [isPlaying, currentTime])
 
   return (
+
     <div className={styles.container} style={{ backgroundImage: `url(background/nordic1.jpg)` }}>
+
+      <div><YouTube opts={opts} onReady={onReady} /></div>
+
       <Head>
         <title>Bragi</title>
         <link rel="icon" href="/favicon.ico" />
@@ -66,7 +94,7 @@ export default function Home() {
         <section className={styles.controls}>
           <img src="img/skipBack.svg" alt="Back" />
           {isPlaying ? (
-            <img src="img/pause.svg" alt="Pause" onClick={stopToPlay} />
+            <img src="img/pause.svg" alt="Pause" onClick={pauseToPlay} />
           ) : (
             <img src="img/play.svg" alt="Play" onClick={startToPlay} />
           )}
@@ -96,7 +124,7 @@ export default function Home() {
             :
             {totalSecondLeft}
             {totalSecondRight}
-            </span>
+          </span>
         </section>
       </main>
 
